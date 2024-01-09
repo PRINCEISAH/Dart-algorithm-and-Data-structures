68e4e8e9177b653ad5408ead630fefa03a05e516
audu isah <37813957+PRINCEISAH@users.noreply.github.com>
2024-01-09 08:58:11 +0100

    solution for predict age challange

--- lib/CodeWarChallange/challenges/predictAge.dart
+++ lib/CodeWarChallange/challenges/predictAge.dart
@@ -1,4 +1,4 @@
-
+import 'dart:math';
 // My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
 
 // In honor of my grandfather's memory we will write a function using his formula!
@@ -10,3 +10,13 @@
 // Divide by two.
 // Example
 // predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+
+int predictAge(List<int> ages) {
+  // Multiply each age by itself and sum the results
+  int sumOfSquares = ages.map((age) => age * age).reduce((a, b) => a + b);
+
+  // Take the square root of the sum and divide by two
+  int predictedAge = (sqrt(sumOfSquares) / 2).toInt();
+
+  return predictedAge;
+}
