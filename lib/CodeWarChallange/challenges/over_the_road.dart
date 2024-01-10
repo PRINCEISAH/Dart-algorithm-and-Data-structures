7635eb765ceba0c66bd236a80dd63dccdb44ecd2
audu isah <37813957+PRINCEISAH@users.noreply.github.com>
2024-01-10 18:23:12 +0100

    added solution for over the road

---
+++ lib/CodeWarChallange/challenges/over_the_road.dart
@@ -0,0 +1,26 @@
+// You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
+
+// Street
+// 1|   |6
+// 3|   |4
+// 5|   |2
+//   you
+// Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
+
+// Example (address, n --> output)
+// Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+// 1, 3 --> 6
+// 3, 3 --> 4
+// 2, 3 --> 5
+// 3, 5 --> 8
+
+int overTheRoad(int address, int n) {
+  // Calculate the total number of houses
+  int totalHouses = 2 * n;
+
+  // Calculate the house number on the opposite side
+  int oppositeAddress = totalHouses - address + 1;
+
+  return oppositeAddress;
+}
